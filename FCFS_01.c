7f18efb707bb1e7206664a90b5613a94a0609fd2
Suprabhat <sdk26150@gmail.com>
2024-09-26 08:42:01 +0530

    Create FCFS_01.c
    First come first served but no tarn around time

---
+++ FCFS_01.c
@@ -0,0 +1,34 @@
+// FCFS with out tarn around time
+
+
+
+#include<stdio.h>
+
+int main() {
+    int max = 30;
+    int n, bt[max], wt[max];
+    float awt = 0;
+    printf("enter the number of process");
+    scanf("%d", &n);
+    printf("Enter the bust time the process");
+    for (int i = 0; i < n; i++)
+    {
+       scanf("%d",&bt[i]);
+    }
+    printf("process\t bust time\t waiting time\n");
+    for (int i = 0; i < n; i++)
+    {
+        wt[i] = 0;
+        for (int j = 0; j < i; j++)
+        {
+            wt[i]=wt[i]+bt[j];
+        }
+         awt=awt+wt[i];
+         printf("%d\t\t%d\t\t%d\n",i+1,bt[i],wt[i]);
+        
+    }
+    awt=awt/n;
+    printf("\nAvarage waiting time = %f",awt);
+
+    return 0;
+}
