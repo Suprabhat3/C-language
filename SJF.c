616ee4d2428209a6f235708f706cfe970e7473f0
Suprabhat <sdk26150@gmail.com>
2024-10-04 11:49:06 +0530

    Update SJF1.c

--- SJF.c
+++ SJF.c
@@ -3,19 +3,18 @@
 #include<stdio.h>
 
 int main() {
-    int max = 30;
-    int n,t,p[max],bt[max],wt[max],tat[max];
-    float awt=0,atat=0;
+    int n;
     printf("enter the number of process :");
     scanf("%d",&n);
-    printf("enter the process number :");
+    int t,p[n],bt[n],wt[n];
+    float awt=0;
+        
     for (int i = 0; i < n; i++)
-    {
+    {   printf("enter the process number :");
         scanf("%d",&p[i]);
     }
-    printf("enter the bust time of the process :");
     for (int i = 0; i < n; i++)
-    {
+    {   printf("enter the burst time of the process %d :",p[i]);
         scanf("%d",&bt[i]);
     }
     // applaying bubble short technique
@@ -27,35 +26,26 @@ int main() {
                 t = bt[j];
                 bt[j] = bt[j+1];
                 bt[j+1] = t;
-                
                 t = p[j];
                 p[j] = p[j+1];
                 p[j+1] = t;
-
-
             }
         }
         
     }
-    printf("process \t bust time \t waiting time \t turn around time \n");
+    printf("process \t bust time \t waiting time\n");
     for (int i = 0; i < n; i++)
     {
         wt[i] = 0;
-        tat[i] = 0;
        for (int j = 0; j < i; j++)
        {
         wt[i] = wt[i]+bt[j];
 
        }
-       tat[i] = wt[i]+bt[i];
        awt = awt + wt[i];
-       atat = atat+tat[i];
-       printf("%d \t\t %d \t\t\t %d \t\t %d\n",p[i],bt[i],wt[i],tat[i]);
+       printf("%d \t\t %d \t\t\t %d\n",p[i],bt[i],wt[i]);
     }
     awt=awt/n;
-    atat=atat/n;
     printf("Avarage waiting time = %f\n",awt);
-    printf("avarage turn around time =%f",atat);
-    
     return 0;
 }
